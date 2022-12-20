b554d88d220704da29769d3d9a86e2cd61cdfc95
siyamulislam <siyamulislam1@gmail.com>
2022-12-20 20:13:30 +0600

    posts crud using controller

---
+++ app/Http/Controllers/PostsApiController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostsApiController extends Controller
+{
+    public function index()
+    {
+        return Post::all();
+    }
+
+    public function store()
+    {
+        request()->validate([
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        return Post::create([
+            'title' => request('title'),
+            'content' => request('content'),
+        ]);
+    }
+
+    public function update(Post $post)
+    {
+        request()->validate([
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        $success = $post->update([
+            'title' => request('title'),
+            'content' => request('content')
+        ]);
+        return ['result' => $success];
+    }
+
+    public function delete(Post $post)
+    {
+        $success = $post->delete();
+        return ['result' => $success];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,54 +3,50 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use App\Http\Controllers\PostsApiController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/posts',function (){
-    return Post::all();
-});
-Route::post('/posts',function (){
+Route::get('/posts',                [PostsApiController::class, 'index'])    ->name('post.index');
+Route::post('/posts',               [PostsApiController::class, 'store'])    ->name('post.store');
+Route::put('posts/update/{post}',   [PostsApiController::class, 'update'])   ->name('post.update');
+Route::delete('posts/delete/{post}',[PostsApiController::class, 'delete'])   ->name('post.delete');
 
-    request()->validate([
-        'title' => 'required',
-        'content' => 'required',
-    ]);
 
-    return Post::create([
-        'title' => request('title'),
-        'content' => request('content'),
-    ]);
-});
+//Route::get('/posts',function (){
+//    return Post::all();
+//});
 
-Route::put('posts/{post}',function (Post $post){ 
-    request()->validate([
-        'title' => 'required',
-        'content' => 'required',
-    ]);
+//Route::post('/posts',function (){
+//
+//    request()->validate([
+//        'title' => 'required',
+//        'content' => 'required',
+//    ]);
+//
+//    return Post::create([
+//        'title' => request('title'),
+//        'content' => request('content'),
+//    ]);
+//});
 
-   $success= $post->update([
-        'title'=>request('title'),
-        'content'=>request('content'),
-    ]);
-    return ['result'=> $success];
-});
+//Route::put('posts/update/{post}',function (Post $post){
+//    request()->validate([
+//        'title' => 'required',
+//        'content' => 'required',
+//    ]);
+//
+//   $success= $post->update([
+//        'title'=>request('title'),
+//        'content'=>request('content'),
+//    ]);
+//    return ['result'=> $success];
+//});
 
-Route::delete('posts/{post}',function (Post $post){ 
-
-   $success= $post->delete();
-   
-    return ['result'=> $success];
-});
+//Route::delete('posts/delete/{post}',function (Post $post){
+//   $success= $post->delete();
+//    return ['result'=> $success];
+//});
